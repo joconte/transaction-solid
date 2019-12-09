dff0f67fe3654b8cd34cc17b990df05608e65cd1
joconte <etud@contejonathan.com>
2019-12-09 19:48:11 +0100

    secure last random

--- src/main/java/fr/epsi/jconte/service/impl/TransactionRandom.java
+++ src/main/java/fr/epsi/jconte/service/impl/TransactionRandom.java
@@ -3,11 +3,20 @@ package fr.epsi.jconte.service.impl;
 import fr.epsi.jconte.model.IPerson;
 import fr.epsi.jconte.service.ITransaction;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class TransactionRandom implements ITransaction {
+
+    private SecureRandom random = SecureRandom.getInstanceStrong();
+
+    public TransactionRandom() throws NoSuchAlgorithmException {
+    }
+
     @Override
     public void makeTransaction(IPerson person1, IPerson person2) {
         double total = person1.getWealth() + person2.getWealth();
-        double newPerson1Wealth = Math.random() * total;
+        double newPerson1Wealth = random.nextDouble() * total;
         double newPerson2Wealth = total - newPerson1Wealth;
 
         person1.setWealth(newPerson1Wealth);
